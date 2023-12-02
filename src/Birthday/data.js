a8cb771f416710c306db7eedcb4abe28d4875749
RoopsDeveloper <rupalisagare7575@gmail.com>
2023-12-02 09:52:58 +0530

    my first commit reactproject

---
+++ src/Birthday/data.js
@@ -0,0 +1,48 @@
+export default [
+    {
+        id: 1,
+        name:"Akshay kumar",
+        age:53,
+        image:"https://th.bing.com/th/id/OIP.tRttNOi7htzp4d43TDkuhQHaLH?pid=ImgDet&rs=1"
+      },
+      {
+        id: 2,
+        name:"Ajay devgan",
+        age:54,
+        image:"https://tse2.mm.bing.net/th?id=OIP.E1EEoIodoqTksQGL5KRsPgHaGv&pid=Api&P=0&h=180"
+      },
+      {
+        id: 3,
+        name:"Rocking Yash",
+        age:34,
+        image:"https://th.bing.com/th/id/OIP.hF6gnD_3l08_Agrcu0S9zgHaEL?w=263&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+      },
+      {
+        id: 4,
+        name:"rajni Kant",
+        age:72,
+        image:"https://th.bing.com/th/id/OIP.kJu-WOoBEXNQ8JgHP0zfPwHaKy?w=119&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+      },
+      {
+        id: 5,
+        name:"Amitab Bachchan",
+        age:81,
+        image:"https://th.bing.com/th/id/OIP.TUlnVww0KmhAELqMrawbgwHaHa?w=194&h=194&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+      },
+      {
+        id: 6,
+        name:"Vijay Joseph",
+        age:49,
+        image:"https://th.bing.com/th/id/OIP.pI7NVIELNiT6LU5hd_aL0wAAAA?w=142&h=210&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+      },
+      {
+        id: 7,
+        name:"Rajkumar",
+        age:69,
+        image:"https://th.bing.com/th/id/OIP.m2ewQCIgbqBlcc-S4UcA9QHaFx?w=211&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+      }
+  
+  ];
+
+//   var(--grey-50)
+// var(--backgroundColor)
